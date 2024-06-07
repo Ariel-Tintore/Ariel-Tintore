c78000a35557b1c1ddf4c02477bcb0f12c5e86d0
Ariel-Tintore <ariielmay27@gmail.com>
2024-06-07 14:08:32 -0300

    Novos comentários

--- treino.py
+++ treino.py
@@ -3,3 +3,10 @@ if idade >= 18:
     print("Você é maior de idade")
 else: print("Você é menor de idade")
 
+# git add tab
+# git commit -m "Colocar a mudança"
+# git push -u origin main - para enviar as alterações para o github
+# git pull puxa todas as alterações feitas pelo github
+# git fetch permite verificar qual a mudança vai ser puxada
+# logo após usar o git fetch, usar o git diff origin/main
+# CTRL + S para salvar as alterações.
