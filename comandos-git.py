fe3a64776e9319bdca16f08208e63ff9f51c77dc
Ariel-Tintore <ariielmay27@gmail.com>
2024-06-07 14:42:05 -0300

    Alocando comandos git

---
+++ comandos-git.py
@@ -0,0 +1,12 @@
+idade = int(input("Quantos anos você tem? "))
+if idade >= 18:
+    print("Você é maior de idade")
+else: print("Você é menor de idade")
+
+# git add tab
+# git commit -m "Colocar a mudança"
+# git push -u origin main - para enviar as alterações para o github
+# git pull puxa todas as alterações feitas pelo github
+# git fetch permite verificar qual a mudança vai ser puxada
+# logo após usar o git fetch, usar o git diff origin/main
+# CTRL + S para salvar as alterações.
