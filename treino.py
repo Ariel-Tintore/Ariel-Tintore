8d00f74cf96458af2b0a75973682e4e01fd9bac0
Ariel-Tintore <ariielmay27@gmail.com>
2024-06-09 11:43:12 -0300

    Limpando

--- treino.py
+++
@@ -1,8 +0,0 @@
-# git add tab
-# git commit -m "Colocar a mudança"
-# git push -u origin main - para enviar as alterações para o github
-# git pull puxa todas as alterações feitas pelo github
-# git fetch permite verificar qual a mudança vai ser puxada
-# logo após usar o git fetch, usar o git diff origin/main
-# CTRL + S para salvar as alterações.
-# comentário
