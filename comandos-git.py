a71c76493e5a6ab4d68840e6dc71a1089dbe94d0
Ariel-Tintore <ariielmay27@gmail.com>
2024-06-09 12:49:46 -0300

    Novos comandos

--- comandos-git.py
+++ comandos-git.py
@@ -5,3 +5,7 @@
 # git fetch permite verificar qual a mudança vai ser puxada
 # logo após usar o git fetch, usar o git diff origin/main
 # CTRL + S para salvar as alterações.
+# git branch "nome da nova branch" para adicionar outra branch sem parenteses 
+# git log --oneline --decorate para ver em qual branch está
+# git checkout "nome da branch que quer ir" sem paranteses
+# .gitignore para criar uma pasta a ser ignorada
